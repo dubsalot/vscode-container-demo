7a9f5f015e350d5996cdc9670dd5639804c16d95
wes <wes.goodwin@gmail.com>
2020-12-01 05:01:12 +0000

    bring over a project created in VS

--- webapp/Startup.cs
+++ webapp/Startup.cs
@@ -9,11 +9,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace webapp
-{
+{  
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        //.
         public void ConfigureServices(IServiceCollection services)
         {
         }
